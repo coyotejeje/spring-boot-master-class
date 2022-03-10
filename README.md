0d09c0a18848c00c61b3110bfa146d13959f21bc
jerome <micl.jerome@gmail.com>
2022-03-10 23:36:28 +0100

    updated READ.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-boot-master-class
+
+Tutoriel d'apprentissage concernant Spring Boot (niveau avancé).
+
+Contenu proposé par Amigoscode
---
+++ src/test/java/fr/coyotejeje/demo/SpringBootMasterClassApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.coyotejeje.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMasterClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
